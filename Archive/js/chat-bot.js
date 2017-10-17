c42573568a44be7e1fd18448aa51326201025e88
Luis Franco <luis_fm_95@live.com.mx>
2017-10-17 16:59:41 -0700

    velga men

--- Archive/js/chat-bot.js
+++ Archive/js/chat-bot.js
@@ -20,20 +20,20 @@ function chatBot() {
 		if(this.match('(hi|hello|hey|hola|howdy)(\\s|!|\\.|$)'))
 			return "Hey!";
 
-		if(this.match('what[^ ]* up') || this.match('sup') || this.match('how are you'))
-			return "this github thing is pretty cool, huh?";
+		if (this.match('no'))
+		{
+			var answer = ["Oh, a newcomer, how exciting! Sit back, this is gonna be awesome!", "Let me ask you some questions:", "What kind of movies do you watch?"];
+			return answer;
+		}
 
-		if(this.match('l(ol)+') || this.match('(ha)+(h|$)') || this.match('lmao'))
-			return "what's so funny?";
+		if (this.match('sci-fi and action'))
+			return "Cool! What is your favorite tv series?";
 
-		if(this.match('^no+(\\s|!|\\.|$)'))
-			return "don't be such a negative dude :(";
-
-		if(this.match('(cya|bye|see ya|ttyl|talk to you later)'))
-			return ["alright, see you around", "good teamwork!"];
-
-		if(this.match('(dumb|stupid|is that all)'))
-			return ["hey i'm just a proof of concept", "you can make me smarter if you'd like"];
+		if (this.match("game of thrones"))
+		{
+			var answer = ["Oh yes, winter is coming! :)", "I have some great ideas for you.", "Berserk from Kentarō Miura"];
+			return answer;
+		}
 
 		if(this.input == 'noop')
 			return;
--- Archive/js/index.js
+++ Archive/js/index.js
@@ -30,15 +30,31 @@ $(function() {
 		$('.busy').css('display', 'block');
 		waiting++;
 		setTimeout( function() {
-			if(typeof reply === 'string') {
+			if(typeof reply === 'string')
 				updateChat(robot, reply);
-			} else {
-				for(var r in reply) {
-					updateChat(robot, reply[r]);
+			else
+				for(var i = 0; i < reply.length; i++)
+				{
+					updateChat(robot, reply[i]);
+					if (reply[i] == 'Berserk from Kentarō Miura')
+					{
+						updateChat2(robot, "Set in a medieval Europe-inspired dark fantasy world, the story centers on isolation, camaraderie, and the question of whether humanity is fundamentally good or evil. Graphic violence and sexual content pervade the story.");
+						updateChat(robot, "Are you tempted? Just click on the picture and start reading!");
+						updateChat(robot, "Otherwise don’t worry, I have some other ideas for you.");
+					}
 				}
-			}
 			if(--waiting == 0) $('.busy').css('display', 'none');
 		}, latency);
+	}
+
+	var display_image = function(src, width, height, alt)
+	{
+		var img = document.createElement("img");
+		img.src = 'img/berserk.jpg';
+		img.width = 150;
+		img.height = 250;
+		img.alt = 'berserk';
+		chat.append(img);
 	}
 
 	// add a new line to the chat
@@ -49,6 +65,7 @@ $(function() {
 			style = 'other';
 		}
 
+
 		var line = $('<div class="row"><span class="party "></span> <span class="text"></span></div>');
 		line.find('.party').addClass(style).text(party + ':');
 		line.find('.text').addClass(style).text(party + ':');
@@ -56,9 +73,33 @@ $(function() {
 
 		chat.append(line);
 
+		if (text == 'Berserk from Kentarō Miura')
+		{
+			display_image('img/berserk.jpg', 150, 250, 'berserk');
+		}
+
 		chat.stop().animate({ scrollTop: chat.prop("scrollHeight")});
 
 	}
+
+	var updateChat2 = function(party, text) {
+
+		var style = 'you';
+		if(party != you) {
+			style = 'other';
+		}
+
+
+		var line = $('<div class="row"><span class="party "></span> <div class="text"></div></div>');
+		line.find('.party').addClass(style).text(party + ':');
+		line.find('.text').addClass(style).text(party + ':');
+		line.find('.text').text(text);
+
+		chat.append(line);
+		chat.stop().animate({ scrollTop: chat.prop("scrollHeight")});
+
+	}
+
 
 	// event binding
 	$('.input').bind('keydown', function(e) {
@@ -69,6 +110,7 @@ $(function() {
 	$('.input a').bind('click', submitChat);
 
 	// initial chat state
-	updateChat(robot, 'Hi there. Have you read any manga before?');
-
+	updateChat(robot, "Hi, I'm Navee, nice to e-meet you!");
+	updateChat(robot, "I'm Here to help you pick the best manga for you.");
+	updateChat(robot, "First of all, are you a manga reader yet?");
 });
